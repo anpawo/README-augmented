573f8cc41b75bc02d3aa1e007a2acbf21011b2cf
marius rousset <rousset.marius13@gmail.com>
2024-01-31 18:21:08 +0100

    feat: not movable tickets

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,13 +1,64 @@
-const TO_DO = 0;
-const IN_PROGRESS = 1;
-const DONE = 2;
-
-type Ticket_s = {
+export type Ticket_s = {
     title: string;
-    text: string;
-    state: 0 | 1 | 2;
 };
 
-const Ticket = (ticket: Ticket_s) => {
-    return <div>hello</div>;
+export type MovingTicket_s = {
+    ticket: Ticket_s;
+    x: number;
+    y: number;
 };
+
+type TicketProps = {
+    tickets: Ticket_s[];
+    state: 0 | 1 | 2;
+    setActiveTicket: React.Dispatch<
+        React.SetStateAction<MovingTicket_s | undefined>
+    >;
+    setTickets: React.Dispatch<React.SetStateAction<Ticket_s[]>>;
+};
+
+const Ticket = ({
+    tickets,
+    state,
+    setActiveTicket,
+    setTickets,
+}: TicketProps) => {
+    return (
+        <div>
+            {tickets.map((ticket, index) => {
+                return (
+                    <div
+                        className={`fixed text-center w-[15%] -translate-x-1/2 border-2 border-black rounded-lg
+                            ${
+                                state == 0
+                                    ? "left-1/3"
+                                    : state == 1
+                                    ? "left-1/2"
+                                    : "left-2/3"
+                            }
+                            top-[${(index * 10).toString()}%]
+                                    `}
+                        key={index}
+                        onClick={event => {
+                            setActiveTicket({
+                                ticket: tickets[index],
+                                x: 0,
+                                y: 0,
+                            });
+                            setTickets(prev =>
+                                prev.filter((_, i) => i !== index)
+                            );
+                        }}
+                        onMouseEnter={event => {
+                            console.log(`in ${state} ${index}`);
+                        }}
+                    >
+                        {ticket.title}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Ticket;
