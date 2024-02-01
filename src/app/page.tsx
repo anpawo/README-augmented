e07ba72a96184388133d7a8704140e5d4634d290
marius rousset <rousset.marius13@gmail.com>
2024-02-01 03:57:54 +0100

    feat: moving ticket, error when adding it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,73 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Ticket, { MovingTicket_s, Ticket_s } from "@/components/Ticket";
 
 export default function Home() {
-    const [ticketsToDo, setTicketsToDo] = useState<Ticket_s[]>([
+    const [ticketsToDo, setTicketsToDo] = useState<(Ticket_s | undefined)[]>([
+        { title: "hello 1" },
+        { title: "hello 2" },
+        { title: "hello 3" },
+    ]);
+    const [ticketsInProgress, setTicketsInProgress] = useState<
+        (Ticket_s | undefined)[]
+    >([{ title: "hello 1" }, { title: "hello 2" }]);
+    const [ticketsDone, setTicketsDone] = useState<(Ticket_s | undefined)[]>([
         { title: "hello" },
     ]);
-    const [ticketsInProgress, setTicketsInProgress] = useState<Ticket_s[]>([
-        { title: "hello" },
-    ]);
-    const [ticketsDone, setTicketsDone] = useState<Ticket_s[]>([
-        { title: "hello" },
-    ]);
-
+    const [ticketToAdd, setTicketToAdd] = useState<Ticket_s | undefined>(
+        undefined
+    );
     const [activeTicket, setActiveTicket] = useState<
         MovingTicket_s | undefined
     >(undefined);
+
+    useEffect(() => {
+        console.log(activeTicket, ticketToAdd);
+        if (ticketToAdd) {
+            console.log("adding ticket");
+            setActiveTicket(undefined);
+            setTicketsToDo(prev => [...prev, ticketToAdd]);
+        }
+    }, [ticketToAdd]);
+
+    useEffect(() => {
+        setTicketToAdd(undefined);
+    }, [ticketsToDo, ticketsInProgress, ticketsDone]);
+
+    const getZone = (event: MouseEvent) => {
+        if (
+            window.innerWidth * 0.25 < event.clientX &&
+            event.clientX < window.innerWidth * 0.41
+        ) {
+            return 0;
+        }
+        return -1;
+    };
+
+    useEffect(() => {
+        document.addEventListener("mouseup", event => {
+            if (!activeTicket) {
+                return;
+            }
+            const zone = getZone(event);
+            if (zone == 0) {
+                setTicketToAdd(activeTicket.ticket);
+            }
+        });
+        document.addEventListener("mousemove", event => {
+            if (!activeTicket) {
+                return;
+            }
+            // inZone(event);
+            setActiveTicket(prev => ({
+                mouseStartX: prev?.mouseStartX,
+                mouseStartY: prev?.mouseStartY,
+                x: event.clientX,
+                y: event.clientY,
+                ticket: prev?.ticket,
+            }));
+        });
+    }, []);
 
     const updateTickets = async () => {
         try {
@@ -74,6 +126,25 @@ export default function Home() {
                     setActiveTicket={setActiveTicket}
                     setTickets={setTicketsDone}
                 />
+            </div>
+            <div>
+                {activeTicket && (
+                    <div
+                        style={{
+                            left: `${
+                                activeTicket?.x - activeTicket?.mouseStartX
+                            }px`,
+                            top: `${
+                                activeTicket?.y - activeTicket?.mouseStartY
+                            }px`,
+                        }}
+                        className={` hover:cursor-pointer h-12 fixed text-center w-[15%] border border-black rounded-md bg-white shadow-md`}
+                    >
+                        <div className="flex justify-center items-center h-full w-full">
+                            {activeTicket?.ticket?.title}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -3,18 +3,20 @@ export type Ticket_s = {
 };
 
 export type MovingTicket_s = {
-    ticket: Ticket_s;
-    x: number;
-    y: number;
+    ticket: Ticket_s | undefined;
+    x: number | undefined;
+    y: number | undefined;
+    mouseStartX: number | undefined;
+    mouseStartY: number | undefined;
 };
 
 type TicketProps = {
-    tickets: Ticket_s[];
+    tickets: (Ticket_s | undefined)[];
     state: 0 | 1 | 2;
     setActiveTicket: React.Dispatch<
         React.SetStateAction<MovingTicket_s | undefined>
     >;
-    setTickets: React.Dispatch<React.SetStateAction<Ticket_s[]>>;
+    setTickets: React.Dispatch<React.SetStateAction<(Ticket_s | undefined)[]>>;
 };
 
 const Ticket = ({
@@ -28,7 +30,10 @@ const Ticket = ({
             {tickets.map((ticket, index) => {
                 return (
                     <div
-                        className={`fixed text-center w-[15%] -translate-x-1/2 border-2 border-black rounded-lg
+                        style={{
+                            top: `${window.innerHeight * 0.175 + index * 56}px`,
+                        }}
+                        className={` hover:cursor-pointer h-12 fixed text-center w-[15%] -translate-x-1/2 border border-black rounded-md bg-white shadow-md
                             ${
                                 state == 0
                                     ? "left-1/3"
@@ -39,21 +44,36 @@ const Ticket = ({
                             top-[${(index * 10).toString()}%]
                                     `}
                         key={index}
-                        onClick={event => {
+                        onMouseDown={event => {
+                            document.body.style.userSelect = "none";
+                            const targetRectangle =
+                                event.target.getBoundingClientRect();
+                            const mouseStartX =
+                                event.clientX - targetRectangle.left;
+                            const mouseStartY =
+                                event.clientY - targetRectangle.top;
+
+                            // Now mouseX and mouseY represent the click position relative to the element.
+                            console.log(
+                                `${targetRectangle.left}, ${targetRectangle.top}`
+                            );
+                            console.log(`${mouseStartX}, ${mouseStartY}.`);
+
                             setActiveTicket({
                                 ticket: tickets[index],
-                                x: 0,
-                                y: 0,
+                                x: targetRectangle.left + mouseStartX,
+                                y: targetRectangle.top + mouseStartY,
+                                mouseStartX: mouseStartX,
+                                mouseStartY: mouseStartY,
                             });
                             setTickets(prev =>
                                 prev.filter((_, i) => i !== index)
                             );
                         }}
-                        onMouseEnter={event => {
-                            console.log(`in ${state} ${index}`);
-                        }}
                     >
-                        {ticket.title}
+                        <div className="flex justify-center items-center h-full w-full">
+                            {ticket?.title}
+                        </div>
                     </div>
                 );
             })}
